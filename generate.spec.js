fa54856e9d2f9969bb4d5f6c8fcec381838c4b42
Jon Fazzaro <jon@fazzaro.com>
2022-06-15 17:09:54 -0400

    wip

--- generate.spec.js
+++ generate.spec.js
@@ -1,27 +1,29 @@
 const generator = require("./generate");
 
 describe("The payment generator", () => {
-  describe("given a date in the middle of the year", () => {
-    it("adds months in the same year", () => {
-      expectDateWhenAddingMonthsTo("5/23/2015", 3, "8/23/2015");
+  describe("when adding months", () => {
+    describe("given a date in the middle of the year", () => {
+      it("adds months in the same year", () => {
+        expectDateWhenAddingMonthsTo("5/23/2015", 3, "8/23/2015");
+      });
     });
-  });
 
-  describe("given a date near the end of the year", () => {
-    it("rolls over to January of the next year", () => {
-      expectDateWhenAddingMonthsTo("11/12/2021", 2, "1/12/2022");
+    describe("given a date near the end of the year", () => {
+      it("rolls over to January of the next year", () => {
+        expectDateWhenAddingMonthsTo("11/12/2021", 2, "1/12/2022");
+      });
     });
-  });
 
-  describe("given the target date is at the end of February", () => {
-    it("still lands in February", () => {
-      expectDateWhenAddingMonthsTo("1/31/2021", 1, "2/28/2021");
+    describe("given the target date is at the end of February", () => {
+      it("still lands in February", () => {
+        expectDateWhenAddingMonthsTo("1/31/2021", 1, "2/28/2021");
+      });
     });
-  });
 
-  function expectDateWhenAddingMonthsTo(date, months, expected) {
-    expect(generator.addMonths(new Date(date), months)).toEqual(
-      new Date(expected)
-    );
-  }
+    function expectDateWhenAddingMonthsTo(date, months, expected) {
+      expect(generator.addMonths(new Date(date), months)).toEqual(
+        new Date(expected)
+      );
+    }
+  });
 });
