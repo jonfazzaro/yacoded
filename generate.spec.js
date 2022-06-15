8e5b8ccb052f7a2fd3a7f557460bee03c67292a2
Jon Fazzaro <jon@fazzaro.com>
2022-06-15 16:57:21 -0400

    wip

--- generate.spec.js
+++ generate.spec.js
@@ -3,9 +3,9 @@ const generator = require("./generate");
 describe("The payment generator", () => {
   describe("given a date before November", () => {
     it("adds two months", () => {
-      const today = new Date(2015, 9, 23);
+      const today = new Date(2015, _months.indexOf("October"), 23);
       const twoMonthsHence = generator.addMonths(today, 2);
-      expect(twoMonthsHence.getMonth()).toEqual(11);
+      expect(_months[twoMonthsHence.getMonth()]).toEqual("December");
       expect(twoMonthsHence.getYear()+1900).toEqual(2015);
     });
   });
