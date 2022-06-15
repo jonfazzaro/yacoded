2272e9b8e2914d40a5f2ded3e213971efaf1f081
Jon Fazzaro <jon@fazzaro.com>
2022-06-15 16:42:48 -0400

    hello

---
+++ generate.js
@@ -0,0 +1,56 @@
+output.markdown('# Payments');
+
+let latest = await latestPaymentDate()
+
+output.markdown(`Payments were recorded last on ${latest.toLocaleDateString()}.`)
+
+let generate = await input.buttonsAsync("Would you like to generate payments for the next month?", ["Yes", "No"])
+if (generate == "Yes") {
+    let date = addMonths(latest, 1)
+    output.markdown(`Adding payments for ${date.toLocaleDateString()}...`)
+    await generatePayments(date)
+}
+
+output.markdown("Have a nice day!")
+
+//////
+
+async function latestPaymentDate() {
+
+    let query = await base.getTable("Payments").selectRecordsAsync({
+        fields: ['Date'],
+        sorts: [{
+            field: "Date",
+            direction: "desc"
+        }]
+    })
+
+    return new Date(query.records[0].getCellValue("Date"))
+
+}
+
+
+async function generatePayments(date) {
+    let accounts = await base.getTable("Accounts").getView("Paying").selectRecordsAsync({ fields: [" Payment "] })
+    let payments = accounts.records
+        .filter(account => 0 < account.getCellValue(" Payment "))
+        .map(account => ({
+        fields: {
+            "Date": date,
+            "Amount": account.getCellValue(" Payment "),
+            "Account": [{ id: account.id }]
+        }
+    }))
+    let created = await base.getTable("Payments").createRecordsAsync(payments)
+    output.markdown(`Created ${created.length} of ${payments.length} payments.`)
+}
+
+
+function addMonths(date, months) {
+    var d = date.getDate();
+    date.setMonth(date.getMonth() + +months);
+    if (date.getDate() != d) {
+        date.setDate(0);
+    }
+    return date;
+}
