4633b7d473237c950998a4121b1815be3406e31a
Jon Fazzaro <jon@fazzaro.com>
2022-06-15 16:57:32 -0400

    wip

--- generate.spec.js
+++ generate.spec.js
@@ -6,9 +6,22 @@ describe("The payment generator", () => {
       const today = new Date(2015, _months.indexOf("October"), 23);
       const twoMonthsHence = generator.addMonths(today, 2);
       expect(_months[twoMonthsHence.getMonth()]).toEqual("December");
-      expect(twoMonthsHence.getYear()+1900).toEqual(2015);
+      expect(twoMonthsHence.getYear() + 1900).toEqual(2015);
     });
   });
 });
 
-const _months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const _months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
